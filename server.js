4e4c7cfebd3f95a3a61b120521c373b21761b7a2
Eric Lifka <eric.lifka@gmail.com>
2015-09-07 12:43:57 -0400

    run server and all that

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/* global process */
 require("babel/register");
 
+var PORT = process.env[ 'SERVER_PORT' ] || 8080;
 var app = require('./app/app.js');
+
+app.listen(PORT, function (error) {
+  if (error) {
+    console.error("Error starting server: " + error);
+  } else {
+    console.info("Server listening on port: " + PORT);
+  }
+});
