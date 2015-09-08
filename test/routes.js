1f0545528501e9924f5abdfb4f1260d22f3f39d2
Eric Lifka <eric.lifka@gmail.com>
2015-09-08 09:59:44 -0400

    move content type check in with greenfield type check, for brevity

--- test/routes.js
+++ test/routes.js
@@ -6,20 +6,26 @@ import app from '../app/app';
 
 describe('CRUD Routes', function () {
   describe('GET /people', function () {
-    it('should return a jsonapi content type', function (done) {
+    it('should return a jsonapi collection', function (done) {
       request(app)
         .get('/people')
         .expect('Content-Type', 'application/vnd.api+json; charset=utf-8')
-        .end(done);
-    });
-
-    it('should return a collection', function (done) {
-      request(app)
-        .get('/people')
         .expect({
           data: []
         })
         .end(done);
     });
+  });
+
+  describe('GET /people/1', function () {
+    it('should return a jsonapi resource object', function (done) {
+      request(app)
+        .get('/people/1')
+        .expect('Content-Type', 'application/vnd.api+json; charset=utf-8')
+        .expect({
+          data: {}
+        })
+        .end(done);
+    })
   });
 });
