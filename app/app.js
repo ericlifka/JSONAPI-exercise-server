e2288e62b4e2e50d1b51f7929ad5b5331759c686
Eric Lifka <eric.lifka@gmail.com>
2015-09-08 15:26:26 -0400

    create a store that can be injected into controllers and manipulated in tests

--- app/app.js
+++ app/app.js
@@ -2,11 +2,14 @@ import express from 'express';
 import contentType from './middleware/content-type';
 import CRUDController from './controllers/crud-controller';
 import Person from './models/person';
+import Store from './services/store';
 
 const app = express();
-const PersonController = new CRUDController(Person);
+const store = new Store();
+const PersonController = new CRUDController(Person, store);
 
 app.use(contentType);
 app.use('/', PersonController.getRouter());
 
 export default app;
+export const _store = store;
